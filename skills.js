ea5a22371e08d2a57dd0637c8b89e109aec2c730
16397-Ramco <xyz968545@gmail.com>
2025-02-03 10:28:34 +0000

    copilot 1st commit

---
+++ skills.js
@@ -0,0 +1,17 @@
+// create a calculate function 
+function calculate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            throw new Error('Invalid operator');
+    }
+}
+
+console.log(calculate(2,3,"*"));
